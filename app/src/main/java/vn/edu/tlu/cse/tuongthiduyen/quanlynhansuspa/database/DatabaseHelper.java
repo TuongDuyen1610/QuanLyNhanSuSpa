10e2bdbc0015d2026b8cac04b98e4e563374e5f2
TUONG THI DUYEN <dd161020042gmail.com>
2025-04-06 19:17:34 +0700

    Add missing .java files

---
+++ app/src/main/java/vn/edu/tlu/cse/tuongthiduyen/quanlynhansuspa/database/DatabaseHelper.java
@@ -0,0 +1,19 @@
+package vn.edu.tlu.cse.tuongthiduyen.quanlynhansuspa.database;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.tlu.cse.tuongthiduyen.quanlynhansuspa.R;
+
+public class DatabaseHelper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/vn/edu/tlu/cse/tuongthiduyen/quanlynhansuspa/model/User.java
@@ -0,0 +1,19 @@
+package vn.edu.tlu.cse.tuongthiduyen.quanlynhansuspa.model;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.tlu.cse.tuongthiduyen.quanlynhansuspa.R;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
